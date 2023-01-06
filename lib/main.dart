8e4a6fe23205d5e3767d7bf9e936438094082b12
LiMan <lightme42@yandex.ru>
2023-01-06 18:00:51 +0800

    initial

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'input.dart';
+
+void main() => runApp(const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        primaryColor: Color(0xFF0A0E21),
+      ),
+    );
+  }
+}
